be2fe69f78855809381ae137d22e25169b16c06a
EverViera12 <evertigre10@gmail.com>
2023-05-27 19:12:57 -0600

    se creó un arvhivo llamado login.js para poder separaresa parte del codigo principal y ponerlo aparte.
    ***= REVISAR EL LOGIN YA QUE NO ES ESTABLE =***

--- routers/log/login.js
+++ routers/log/login.js
@@ -1,60 +1,56 @@
-// // login.js
-// const express = require("express");
-// const bcryptjs = require("bcryptjs");
-// const conexion = require("../../dataBase/db");
+const express = require("express");
+const login = express();
 
-// const login = express();
+const conexion = require("../../dataBase/db");
 
-// login.post("/auth", async (req, res) => {
-//   const usuario = req.body.usuario;
-//   const clave = req.body.clave;
-//   let passwordHash = await bcryptjs.hash(clave, 8);
-//   if (usuario && clave) {
-//     conexion.query(
-//       "SELECT * FROM usuarios WHERE usuario = ?",
-//       [usuario],
-//       async (error, results) => {
-//         if (
-//           results.length === 0 ||
-//           (await bcryptjs.compare(clave, results[0].clave))
-//         ) {
-//           res.render("login", {
-//             alert: true,
-//             alertTitle: "Error",
-//             alertMessage: "Usuarios o contraseña incorrectos",
-//             alertIcon: "error",
-//             showConfirmButton: true,
-//             timer: 1500,
-//             ruta: "login",
-//           });
-//         } else {
-//           req.session.loggedin = true;
-//           req.session.nombre = results[0].nombre;
-//           res.render("login", {
-//             alert: true,
-//             alertTitle: "Conectado",
-//             alertMessage: "Bienvenido: " + usuario,
-//             alertIcon: "success",
-//             showConfirmButton: false,
-//             timer: 1500,
-//             ruta: "",
-//           });
-//         }
-//       }
-//     );
-//   } else {
-//     res.render("login", {
-//       alert: true,
-//       alertTitle: "Advertencia",
-//       alertMessage: "Ingresa tus credenciales",
-//       alertIcon: "warning",
-//       showConfirmButton: true,
-//       timer: 15000,
-//       ruta: "login",
-//     });
-//   }
-// });
-// /* ============================================================== */
-// /* ======================== FIN LOGIN =========================== */
-// /* ============================================================== */
-// module.exports = login;
+login.post("/auth", async (req, res) => {
+  const usuario = req.body.usuario;
+  const clave = req.body.clave;
+  // let passwordHash = await bcryptjs.hash(clave, 8);
+  if (usuario && clave) {
+    conexion.query(
+      "SELECT * FROM usuarios WHERE usuario = ?",
+      [usuario],
+      async (error, results) => {
+        if (results[0].usuario === usuario && results[0].clave === clave) {
+          console.log(results[0]);
+          console.log(error);
+
+          req.session.loggedin = true;
+          req.session.nombre = results[0].nombre;
+          res.render("login", {
+            alert: true,
+            alertTitle: "Conectado",
+            alertMessage: "Bienvenido: " + usuario,
+            alertIcon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: "",
+          });
+        } else {
+          res.render("login", {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "Usuarios o contraseña incorrectos",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: 1500,
+            ruta: "login",
+          });
+        }
+      }
+    );
+  } else {
+    res.render("login", {
+      alert: true,
+      alertTitle: "Advertencia",
+      alertMessage: "Ingresa tus credenciales",
+      alertIcon: "warning",
+      showConfirmButton: true,
+      timer: 15000,
+      ruta: "login",
+    });
+  }
+});
+
+module.exports = login;
