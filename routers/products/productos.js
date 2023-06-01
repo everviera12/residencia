0b67e8a23372c4f57cddc510462b00a1414021b8
EverViera12 <evertigre10@gmail.com>
2023-05-31 22:48:00 -0600

    ver toda la carpeta router

--- routers/products/productos.js
+++ routers/products/productos.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const conexion = require("../../dataBase/db");
+// const express = require("express");
+// const productos = express.Router();
+// const conexion = require("../../dataBase/db");
 
-// MUESTRA TODOS LOS PRODUCTOS DE LA BASE DE DATOS
-router.get("/productos", (req, res) => {
-  conexion.query("SELECT * FROM productos", (error, results) => {
-    console.log(results);
-    if (error) {
-      console.log("EL ERROR ES " + error);
-    } else {
-      res.render("productos", { results: results });
-    }
-  });
-});
+// // MUESTRA TODOS LOS PRODUCTOS DE LA BASE DE DATOS
+// productos.get("/productos", (req, res) => {
+//   conexion.query("SELECT * FROM productos", (error, results) => {
+//     if (error) {
+//       console.log("EL ERROR ES " + error);
+//     } else {
+//       res.render("productos", { results: results });
+//     }
+//   });
+// });
 
-module.exports = router;
+
+// module.exports = productos;
